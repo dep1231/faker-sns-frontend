4c4bb2c0ded8b2473dadc685e92743b35cde79b1
shiotani0011 <76035510+kizuneko1@users.noreply.github.com>
2022-06-17 03:11:19 +0900

    docs: build

---
+++ src/config/config.development.js
@@ -0,0 +1,3 @@
+export const API_USER_URL = "http://localhost:8000/api/user/";
+export const API_POST_URL = "http://localhost:8000/api/post/";
+export const API_COMMENT_URL = "http://localhost:8000/api/comment/";
---
+++ src/config/config.production.js
@@ -0,0 +1,4 @@
+export const API_USER_URL = "https://faker-sns-api.herokuapp.com/api/user/";
+export const API_POST_URL = "https://faker-sns-api.herokuapp.com/api/post/";
+export const API_COMMENT_URL =
+  "https://faker-sns-api.herokuapp.com/api/comment/";
